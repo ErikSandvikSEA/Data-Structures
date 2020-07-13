3baa8c162726217913a18db49cd6cd473d94eb6b
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-07-13 12:28:58 -0700

    Stack tests passing for array implementation

--- stack/stack.py
+++ stack/stack.py
@@ -13,15 +13,22 @@ return elements in Last In First Out order.
 
 
 class Stack:
-    def __init__(self, storage=[]):
+    def __init__(self):
         self.size = 0
-        self.storage = storage
+        self.storage = []
 
     def __len__(self):
-        pass
+        return self.size
 
     def push(self, value):
-        pass
+        self.storage.append(value)
+        self.size = len(self.storage)
 
     def pop(self):
-        pass
+        if self.size == 0:
+            return None
+        else:
+            popped = self.storage.pop()
+            self.size = len(self.storage)
+            return popped
+
