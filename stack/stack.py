021d145f9bb8920b08126631c9733d174514338e
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-07-13 15:09:47 -0700

    stack tests pass for array and linked list

--- stack/stack.py
+++ stack/stack.py
@@ -12,22 +12,35 @@ return elements in Last In First Out order.
 """
 
 from stack_singly_linked_list import LinkedList
+from stack_singly_linked_list import Node
 
 
 # LINKED LIST IMPLEMENTATION -------------------------------------------------
 class Stack:
     def __init__(self):
         self.size = 0
-        self.storage = None
+        self.storage = LinkedList()
 
     def __len__(self):
-        pass
+        # count = 0
+        # current = self.storage.head
+        # while current:
+        #     current = current.get_next()
+        #     count += 1
+        # return count
+        return self.size
 
     def push(self, value):
-        pass
+        # set a new node that has the input value
+        self.size += 1
+        return self.storage.add_to_tail(value)
 
     def pop(self):
-        pass
+        if self.size == 0:
+            return None
+        else:
+            self.size -= 1
+            return self.storage.remove_tail()
 
 
 # ARRAY IMPLEMENTATION -----------------------------------------------------
